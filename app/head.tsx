9502c7e8b47e047bbe527fd87323d1c6e0111ebd
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-01-30 22:00:12 +0530

    feat: app dir setup

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Next DC Marvel Project</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Tracks Upcoming DC and Marvel Projects" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
