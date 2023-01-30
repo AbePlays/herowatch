973f8dd0d91d9912416cc5cd99b729108e59d7b3
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-01-30 22:33:49 +0530

    feat: migrate home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+import { Plus_Jakarta_Sans } from '@next/font/google'
+
+import '../styles/globals.css'
+
+const font = Plus_Jakarta_Sans({ subsets: ['latin'] })
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -6,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,46 @@
+import Link from 'next/link'
+
+import dc from '../public/images/DC.png'
+import handshake from '../public/images/Handshake.webp'
+import marvel from '../public/images/Marvel.jpeg'
+import ImageWrapper from './ImageWrapper'
+
 export default function Page() {
-  return <h1>I am homepage</h1>
+  return (
+    <>
+      <div className="relative min-h-screen w-screen bg-[url('/images/bg.jpeg')] bg-cover bg-no-repeat blur-2xl hue-rotate-[150deg]"></div>
+      <div className="absolute inset-0 p-4 text-center">
+        <header>
+          <nav>
+            <Link className="text-lg tracking-widest" href="/">
+              trackr.
+            </Link>
+          </nav>
+        </header>
+        <div className="mt-20">
+          <p className="font-bold text-stone-600" style={{ wordSpacing: '2px' }}>
+            Upcoming DC and Marvel Project Tracker
+          </p>
+          <h1 className="mt-20 text-5xl font-bold leading-tight sm:text-6xl" id="title">
+            Choose a Cinematic <br />
+            Universe
+          </h1>
+        </div>
+        <ul
+          aria-labelledby="title"
+          className="no-scrollbar mx-auto mt-20 flex max-w-screen-md snap-x gap-8 overflow-x-auto p-4"
+        >
+          <li className="snap-center">
+            <ImageWrapper alt="marvel logo" bg="bg-[#ec1d24]" href="/marvel/movie" src={marvel} />
+          </li>
+          <li className="snap-center">
+            <ImageWrapper alt="dc logo" bg="bg-[#0378F2]" href="/dc/movie" src={dc} />
+          </li>
+          <li className="snap-center">
+            <ImageWrapper alt="" bg="bg-[#fff3c1]" href="/all/movie" src={handshake} />
+          </li>
+        </ul>
+      </div>
+    </>
+  )
 }
