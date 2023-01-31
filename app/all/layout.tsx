0e4a4d454e52e00f0b955d7f089d4f72a582de3f
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-01-31 22:45:08 +0530

    feat: add routes for dc and all

---
+++ app/all/layout.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import React from 'react'
+
+import Wrapper from '../../components/Wrapper'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <div className="min-h-screen bg-[#EFEDE9] p-4 sm:px-8">
+        <div className="mx-auto max-w-screen-lg">
+          <header>
+            <nav className="text-center ">
+              <Link href="/" className="text-lg tracking-widest">
+                trackr.
+              </Link>
+            </nav>
+          </header>
+          <Wrapper heading="all">{children}</Wrapper>
+        </div>
+      </div>
+    </>
+  )
+}
--- app/marvel copy/page.tsx
+++ app/marvel copy/page.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export default function Page() {
   return <p>Select movies or tv shows to see more info.</p>
 }
